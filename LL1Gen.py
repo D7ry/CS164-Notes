67c56902041b6b3420f3bc16f6697503c1f38095
Tianyun Yuan <7ianyun@berkeley.edu>
2023-02-11 16:36:39 -0800

    algorithm for follows()

--- LL1Gen.py
+++ LL1Gen.py
@@ -2,17 +2,29 @@
 epsilon = "ε"
 class Grammar:
 
-	def __init__(self, name : str, non_terminals : set, terminals : set):
+	def __init__(self, name : str, non_terminals : set, terminals : set, start : str = "S"):
 		self.productions = dict() # str -> list(list(str))
 		for nt in non_terminals:
 			self.productions[nt] = [] # make a new list for each NT to store productions
 		self.terminals = terminals
+		for term in self.terminals:
+			if term in self.productions:
+				raise Exception("Terminal {} is also a non-terminal.".format(term))
 		#non-terminals is self.productions.keys()
 		self.name = name
+		self.start = start
+		if start not in non_terminals:
+			raise Exception("Start symbol {} not in non-terminals.".format(start))
 		print("Initializing grammar {}.".format(name))
 		print("Non-terminals: ", self.productions.keys())
 		print("Terminals: ", terminals)
 		self.left_recursion_removed = False
+	
+	def non_terminals(self):
+		return self.productions.keys()
+
+	def is_non_terminal(self, symbol:str):
+		return symbol in self.productions.keys()
 
 	# epsilon is indicated using "ε" sign
 	def add_production(self, lhs:str, rhss:list[list[str]]):
@@ -80,10 +92,20 @@ class Grammar:
 	def get_ll1(self):
 		if not self.left_recursion_removed:
 			raise Exception("Left recursion not removed.")
-		firsts = dict()
-		follows = dict()
+		firsts = dict() #dict<str,set>
+		follows = dict() #dict<str,set>
+		
+		#return a copy of firsts[symbol] if symbol is in firsts, otherwise return an empty set
+		def first(symbol) -> set[str]: 
+			if symbol in self.terminals:
+				return symbol
+			return firsts[symbol].copy() if symbol in firsts else set()
 
-		def get_first(lhs:str):
+		#return a copy of follows[symbol] if symbol is in follows, otherwise return an empty set
+		def follow(symbol) -> set[str]:
+			return follows[symbol].copy() if symbol in follows else set()
+
+		def calc_first(lhs:str):
 			firsts[lhs] = set()
 			rhss = self.productions[lhs]
 			for rhs in rhss:
@@ -91,7 +113,7 @@ class Grammar:
 					firsts[lhs].add(epsilon)
 				elif rhs[0] in self.terminals:
 					firsts[lhs].add(rhs[0])
-				else: #non-terminal loop
+				else: #loop through all symbols in rhs
 					toTheEnd = True
 					#loop through all elements in rhs, stop at first terminal, keep going if epsilon
 					#appears in first(Yi), add epsilon in all elenents in rhs have epsilon.
@@ -103,7 +125,7 @@ class Grammar:
 						elif elem == epsilon:
 							firsts[lhs].add(epsilon)
 						else:# elem is y1, y2, y3, etc
-							get_first(elem) if elem not in firsts else None #get first of elem if not already calculated
+							calc_first(elem) if elem not in firsts else None #get first of elem if not already calculated
 							if epsilon not in firsts[elem]: #if epsilon not in first(Y1), then stop
 								firsts[lhs].update(firsts[elem]) #first(X) = first(Y1)
 								toTheEnd = False
@@ -113,11 +135,57 @@ class Grammar:
 							firsts[lhs].update(firsts_copy) #keep going
 					if toTheEnd:
 						firsts[lhs].add(epsilon)
+
+		
 		# firsts
 		for lhs in self.productions.keys():
-			get_first(lhs)
+			calc_first(lhs)
 		print("Firsts:")
 		print(firsts)
+		print()
+
+		# follows
+		follows[self.start] = {"$"} #start symbol
+		links = dict() # symbol -> set of symbols to which the symbol contributes its follow to
+		def link(src, dst):
+			if dst == src: #don't link to itself
+				return
+			if src in links:
+				links[src].add(dst)
+			else:
+				links[src] = {dst}
+		for lhs, rhss in self.productions.items():
+			for rhs in rhss: # rhs = [symbol_1, symbol_2, symbol_3, ...]
+				i = len(rhs) - 1
+				while i >= 0: # go from right to left
+					if self.is_non_terminal(rhs[i]): #terminals don't have follow
+						Yi = rhs[i]
+						if Yi not in follows:
+							follows[Yi] = set()
+						if i == len(rhs) - 1: #last element, link follow of lhs to follow of symbol_i
+							link(lhs, Yi)
+						else: #not last element
+							first_Yi_plus_1 = first(rhs[i+1])
+							if epsilon in first_Yi_plus_1: #if epsilon in first(Yi+1), then link follow of follow(Yi+1) to follow of follow(Yi)(transitive property)
+								link(rhs[i+1], Yi)
+								first_Yi_plus_1.remove(epsilon)
+							follows[Yi].update(first_Yi_plus_1) #follow(Yi) += first(Yi+1)
+					i -= 1
+						
+		#loop through links
+		def update_link(src):
+			for dst in links[src]:
+				print("Linking follow({}) to follow({})".format(src, dst))
+				follows[dst].update(follow(src))
+				if dst in links:
+					print("follow({}) updated, updating links...".format(dst))
+					update_link(dst) #recursively update links
+					print("back to linking follow({})".format(src))
+
+		for src in links.keys():
+				update_link(src)
+		print("Follows:")
+		print(follows)
 			
 				
 		
